5b14d6bce46c7f4ff3331fd69800fc692cc52a5a
Rendi Fadilah 31 <rendifadillah009@gmail.com>
2020-08-27 21:30:02 +0700

    build rest api use lumen

---
+++ app/Produk.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Produk extends Model
+{
+
+    protected $table = 'produk';
+
+    protected $fillable = [
+        'nama', 'harga', 'warna',
+        'kondisi', 'deskripsi'
+    ];
+
+}
